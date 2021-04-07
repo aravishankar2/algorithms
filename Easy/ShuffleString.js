f158b392535a18f8d3ee599991baf78c2b93ce0e
Arun Ravishankar <aravishankar2@gmail.com>
2021-04-07 14:11:38 -0500

    shuffle stirng

---
+++ Easy/ShuffleString.js
@@ -0,0 +1,30 @@
+/*
+Given a string s and an integer 
+array indices of the same length.
+
+The string s will be shuffled such 
+that the character at the ith position 
+moves to indices[i] in the shuffled string.
+
+Return the shuffled string.
+
+*/
+
+
+let strArr = [];
+
+let s = "aiohn";
+
+let indices = [3, 1, 4, 2, 0];
+
+function ShuffleString(s, indices) {
+  let obj = {};
+
+  indices.map((_, i) => {
+    obj[indices[i]] = s[i];
+  });
+
+  return Object.values(obj).join("");
+}
+
+console.log(ShuffleString(s, indices))
