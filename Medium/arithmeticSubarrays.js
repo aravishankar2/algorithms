de99a0474cfa42b1a154d5e0830bbd27e46fd5ad
Arun Ravishankar <aravishankar2@gmail.com>
2021-04-19 07:53:47 -0500

    arithmetic subarrays

---
+++ Medium/arithmeticSubarrays.js
@@ -0,0 +1,89 @@
+/*
+A sequence of numbers is called arithmetic if it 
+consists of at least two elements, and the difference 
+between every two consecutive elements is the same. More formally, 
+a sequence s is arithmetic if and only if s[i+1] - s[i] == s[1] - s[0] 
+for all valid i.
+
+For example, these are arithmetic sequences:
+
+1, 3, 5, 7, 9
+7, 7, 7, 7
+3, -1, -5, -9
+
+The following sequence is not arithmetic:
+1, 1, 2, 5, 7
+
+
+You are given an array of n integers, nums, and two 
+arrays of m integers each, l and r, representing the m 
+range queries, where the ith query is the range [l[i], r[i]]. 
+All the arrays are 0-indexed.
+
+Return a list of boolean elements answer, where answer[i] 
+is true if the subarray nums[l[i]], nums[l[i]+1], ... , nums[r[i]] 
+can be rearranged to form an arithmetic sequence, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: nums = [4,6,5,9,3,7], l = [0,0,2], r = [2,3,5]
+Output: [true,false,true]
+Explanation:
+In the 0th query, the subarray is [4,6,5]. This can be rearranged as [6,5,4], which is an arithmetic sequence.
+In the 1st query, the subarray is [4,6,5,9]. This cannot be rearranged as an arithmetic sequence.
+In the 2nd query, the subarray is [5,9,3,7]. This can be rearranged as [3,5,7,9], which is an arithmetic sequence.
+
+
+
+Example 2:
+
+Input: nums = [-12,-9,-3,-12,-6,15,20,-25,-20,-15,-10], l = [0,1,6,4,8,7], r = [4,4,9,7,9,10]
+Output: [false,true,false,false,true,true]
+ 
+
+Constraints:
+
+    n == nums.length
+    m == l.length
+    m == r.length
+    2 <= n <= 500
+    1 <= m <= 500
+    0 <= l[i] < r[i] < n
+    -105 <= nums[i] <= 105
+
+*/
+
+let arithmetic = (arr) => {
+  let p1 = 0;
+  let p2 = 1;
+  let target = arr[p2] - arr[p1];
+  let ans = [];
+  while (p2 < arr.length) {
+    if (arr[p2] - arr[p1] === target) {
+      ans.push(arr[p2] - arr[p1]);
+      p1 += 1;
+      p2 += 1;
+    } else {
+      return ans.length === arr.length - 1;
+    }
+  }
+  return ans.length === arr.length - 1;
+};
+
+var checkArithmeticSubarrays = (nums, l, r) =>
+  l.map((_, i) => arithmetic(nums.slice(l[i], r[i] + 1).sort((a, b) => a - b)));
+
+let l = [0, 1, 6, 4, 8, 7];
+let r = [4, 4, 9, 7, 9, 10];
+
+console.log(
+  checkArithmeticSubarrays(
+    [-12, -9, -3, -12, -6, 15, 20, -25, -20, -15, -10],
+    l,
+    r
+  )
+);
+
+// Output: [false,true,false,false,true,true]
