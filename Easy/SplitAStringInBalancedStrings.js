1bbbac014f48d13dc44761b5937801b8048753aa
Arun Ravishankar <aravishankar2@gmail.com>
2021-04-24 18:41:11 -0500

    sign of the product of an array

--- Easy/SplitAStringInBalancedStrings.js
+++ Easy/SplitAStringInBalancedStrings.js
@@ -1,6 +1,47 @@
-function peek(stack) {
-  return stack[stack.length - 1];
-}
+/*
+Balanced strings are those that have an equal 
+quantity of 'L' and 'R' characters.
+
+Given a balanced string s, split it in the maximum 
+amount of balanced strings.
+
+Return the maximum amount of split balanced strings.
+
+ 
+
+Example 1:
+
+Input: s = "RLRRLLRLRL"
+Output: 4
+Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+
+
+Example 2:
+
+Input: s = "RLLLLRRRLR"
+Output: 3
+Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
+
+
+Example 3:
+
+Input: s = "LLLLRRRR"
+Output: 1
+Explanation: s can be split into "LLLLRRRR".
+
+
+Example 4:
+
+Input: s = "RLRRRLLRLL"
+Output: 2
+Explanation: s can be split into "RL", "RRRLLRLL", since each substring contains an equal number of 'L' and 'R'
+ 
+
+Constraints:
+  1 <= s.length <= 1000
+  s[i] is either 'L' or 'R'.
+  s is a balanced string.
+*/
 
 var balancedStringSplit = function (s) {
   let r = 0;
@@ -21,4 +62,5 @@ var balancedStringSplit = function (s) {
 
   return ans;
 };
+
 console.log(balancedStringSplit("RLRRRLLRLL"));
